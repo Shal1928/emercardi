d7801493ddea1b56bdf0412b9410e270eb72c5e6
DShalygin@it.ru <MopeMope2222>
2016-08-06 21:29:44 +0700

    Working menu and navigation button from toolbar

--- app/src/main/java/ru/shal1928/emercardi/app/activities/MainActivity.java
+++ app/src/main/java/ru/shal1928/emercardi/app/activities/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import ru.shal1928.emercardi.app.R;
@@ -18,9 +20,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                System.out.println("***1");
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                builder.setTitle("Важное сообщение2!")
+//                    .setMessage("Покормите кота2!")
+//                    .setIcon(R.drawable.ic_create_black_18dp)
+//                    .setCancelable(false)
+//                    .setNegativeButton("ОК, иду на кухню",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                dialog.cancel();
+//                            }
+//                        });
+//                AlertDialog alert = builder.create();
+//                alert.show();
+//            }
+//        });
+
+        toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                System.out.println("***1");
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                 builder.setTitle("Важное сообщение!")
                     .setMessage("Покормите кота!")
@@ -36,11 +60,15 @@ public class MainActivity extends AppCompatActivity {
                 alert.show();
             }
         });
+//        toolbar.setOnMenuItemClickListener();
+//        toolbar.inflateMenu(R.menu.menu_main);
+//        Menu menu = toolbar.getMenu();
+
 
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.ic_menu_black_24dp);
-
+//        getSupportActionBar().setDisplayShowCustomEnabled(true);
+//        getSupportActionBar().setIcon();
+        getSupportActionBar().setElevation(5);
 
 
 
@@ -52,11 +80,53 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 //    @Override
-//    public boolean onOptionsItemSelected(MenuItem menuItem) {
-//        if (menuItem.getItemId() == android.R.id.home) {
-//
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.h:
+//                // Do Activity menu item stuff here
+//                return true;
+//            case R.id.fragment_menu_item:
+//                // Not implemented here
+//                return false;
+//            default:
+//                break;
 //        }
-//        return super.onOptionsItemSelected(menuItem);
+//
+//        return false;
 //    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case android.R.id.home:
+                System.out.println("***");
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Важное сообщение!")
+                    .setMessage("Покормите кота!")
+                    .setIcon(R.drawable.ic_create_black_18dp)
+                    .setCancelable(false)
+                    .setNegativeButton("ОК, иду на кухню",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+                break;
+
+        }
+
+        return true;
+    }
 }
